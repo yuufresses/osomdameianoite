f24ef3502ef75ced298fb68ee82f0cd97bfa3513
yuufresses <168889640+yuufresses@users.noreply.github.com>
2024-05-19 10:04:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,99 @@
+function fase1() 
+{
+
+    // Converte para minúsculas
+    // .value.toLowerCase(); 
+
+    var textoJogador = document.querySelector("#texto").value.toLowerCase();
+    var palavraChave = "eu era determinado".toLowerCase();
+
+    if (textoJogador === palavraChave) 
+    {
+        alert("Parabens!");
+        window.location.href = "1eueradeterminado.html";
+    } 
+    else 
+    {
+        alert("Você Errou!");
+    }
+}
+
+function fase2() 
+{
+    var textoJogador = document.querySelector("#texto").value.toLowerCase();
+    var palavraChave = "minha mãe não me entendia".toLowerCase();
+
+    if (textoJogador === palavraChave) 
+    {
+        alert("Parabens!");
+        window.location.href = "2minhamãenãomeentendia.html";
+    } 
+    else 
+    {
+        alert("Você Errou!");
+    }
+}
+
+function fase3() 
+{
+    var textoJogador = document.querySelector("#texto").value.toLowerCase();
+    var palavraChave = "as vozes da minha cabeça".toLowerCase();
+
+    if (textoJogador === palavraChave) 
+    {
+        alert("Parabens!");
+        window.location.href = "3asvozesdaminhacabeça.html";
+    } 
+    else 
+    {
+        alert("Você Errou!");
+    }
+}
+
+function fase4() 
+{
+    var textoJogador = document.querySelector("#texto").value.toLowerCase();
+    var palavraChave = "eram um pesadelo".toLowerCase();
+
+    if (textoJogador === palavraChave) 
+    {
+        alert("Parabens!");
+        window.location.href = "4eramumpesadelo.html";
+    } 
+    else 
+    {
+        alert("Você Errou!");
+    }
+}
+
+function fase5() 
+{
+    var textoJogador = document.querySelector("#texto").value.toLowerCase();
+    var palavraChave = "Alguem esta me observando".toLowerCase();
+
+    if (textoJogador === palavraChave) 
+    {
+        alert("Parabens!");
+        window.location.href = "PAGINA6.html";
+    } 
+    else 
+    {
+        alert("Você Errou!");
+    }
+}
+
+function fase6() 
+{
+    var textoJogador = document.querySelector("#texto").value.toLowerCase();
+    var palavraChave = "Ele esta aki".toLowerCase();
+
+    if (textoJogador === palavraChave) 
+    {
+        alert("Parabens!");
+        window.location.href = ".html";
+    } 
+    else 
+    {
+        alert("Você Errou!");
+    }
+}
